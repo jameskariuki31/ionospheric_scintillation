bd79dfd064ea162060dda96071bb2b0d21a9cec5
Chege <jameskariuki31@gmail.com>
2020-02-05 09:46:34 +0800

    made code pep8 compliant, added plotting and fitting functions

--- run_ionfr/spatialfr.py
+++ run_ionfr/spatialfr.py
@@ -1,26 +1,26 @@
 import os
 import time
 from datetime import datetime, timedelta
-from dateutil import tz
-from pytz import timezone
-from rad2hmsdms import rad2hmsdms
-import urllib.request
 import yaml
 from yaml import SafeLoader as SafeLoader
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
-from matplotlib import gridspec
 import run_ionfr
 import pandas as pd
 from scipy.interpolate import griddata
+from scipy.optimize import curve_fit
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+
 
 path = '/home/chege/Desktop/curtin_work/vega/'
 
+
 def loadfile(data_file):
     with open(data_file, 'r') as f:
-                unpacked = yaml.load(f, Loader=SafeLoader)
-	
+        unpacked = yaml.load(f, Loader=SafeLoader)
+
     flux = []
     ra = []
     dec = []
@@ -29,47 +29,54 @@ def loadfile(data_file):
     stds = []
     sourcelist = unpacked['sources']
     for source in sourcelist:
-        #print(unpacked['sources'][source]['name'])
+        # print(unpacked['sources'][source]['name'])
         names.append(unpacked['sources'][source]['name'])
         dec.append(unpacked['sources'][source]['dec'])
         ra.append(unpacked['sources'][source]['ra'])
-        flux.append(unpacked['sources'][source]['flux_density']) 
+        flux.append(unpacked['sources'][source]['flux_density'])
         ampscales.append(np.nanmedian(unpacked['sources'][source]['amp_scales']))
         stds.append(np.nanstd(unpacked['sources'][source]['amp_scales']))
-        #print(unpacked2['sources'][source]['amp_scales'])
-    #ampscales = np.nan_to_num(ampscales)
-    #print(len(ampscales))
-    df = pd.DataFrame(list(zip(ra, dec, ampscales, stds, flux, names)), columns=['ra', 'dec', 'ampscales', 'stds', 'flux', 'names'])
+        # print(unpacked2['sources'][source]['amp_scales'])
+    # ampscales = np.nan_to_num(ampscales)
+    # print(len(ampscales))
+    df = pd.DataFrame(
+        list(zip(ra, dec, ampscales, stds, flux, names)),
+        columns=['ra', 'dec', 'ampscales', 'stds', 'flux', 'names'])
     df2 = df.dropna(axis=0)
     return df2
 
 
 def get_utc_time(obsid):
-    os.environ['TZ'] = 'right/UTC' # TAI scale with 1970-01-01 00:00:10 (TAI) epoch
+    # TAI scale with 1970-01-01 00:00:10 (TAI) epoch
+    os.environ['TZ'] = 'right/UTC'
     time.tzset()
-    gps_epoch_as_gps = datetime(1980, 1, 6) 
+    gps_epoch_as_gps = datetime(1980, 1, 6)
 
-    gps_time_as_gps = gps_epoch_as_gps + timedelta(seconds=float(obsid)) 
-    gps_time_as_tai = gps_time_as_gps + timedelta(seconds=19) # constant offset
+    gps_time_as_gps = gps_epoch_as_gps + timedelta(seconds=float(obsid))
+    gps_time_as_tai = gps_time_as_gps + timedelta(seconds=19)  # const offset
     tai_epoch_as_tai = datetime(1970, 1, 1, 0, 0, 10)
     # by definition
-    tai_timestamp = (gps_time_as_tai - tai_epoch_as_tai).total_seconds() 
-    print(datetime.utcfromtimestamp(tai_timestamp)) # "right" timezone is in effect!
+    tai_timestamp = (gps_time_as_tai - tai_epoch_as_tai).total_seconds()
+    print(datetime.utcfromtimestamp(tai_timestamp))   # "right" timezone
     utc_time = datetime.utcfromtimestamp(tai_timestamp)
 
     utc_date = str(utc_time)[0:10]
     print(utc_date)
     return utc_date
 
+
 def get_obsid_ionfr_txts(obsid):
     data_file = '%s' % (path+'%s.yaml' % (str(obsid)))
     df = loadfile(data_file)
     date = get_utc_time(obsid)
     for index, row in df.iterrows():
-        run_ionfr.run_ionfr(row['ra'], row['dec'], row['names'], date, convertradec=True)
+        run_ionfr.run_ionfr(
+            row['ra'], row['dec'], row['names'], date, convertradec=True
+            )
+
 
 def get_fr_value(ionfr_txt_file, hour):
-    with open(ionfr_txt_file) as csvfile: 
+    with open(ionfr_txt_file) as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=' ')
         for row in csv_reader:
             if row[0] == str(hour):
@@ -78,39 +85,48 @@ def get_fr_value(ionfr_txt_file, hour):
                 fr_value_err = row[4]
     return fr_value, fr_value_err
 
-def make_df(hour):
-    thisdir = os.path.abspath('')
-    fyls = os.listdir(thisdir)
-    ras = []
-    decs = []
-    frs = []
-    frs_errs = []
-    for fyl in fyls:
-        print(fyl)
-        if fyl.split('.')[-1] == 'txt':
-            source = fyl.split('_')[0]
-            ra, dec = run_ionfr.get_radec(source)
-            ras.append(float(ra))
-            decs.append(float(dec))
-            fr_value, fr_value_err = get_fr_value(fyl, hour)
-            frs.append(float(fr_value))
-            frs_errs.append(float(fr_value_err))
-    df = pd.DataFrame(list(zip(ras, decs, frs, frs_errs)), columns=['ra', 'dec', 'fr', 'fr_err'])
-    print('made dataframe with radec and fr values')
-    df = df.dropna(axis = 0)
+
+def make_df(hour, csvfyl=None):
+    if csvfyl is not None:  # if we have a csv file with ra dec, fr, and fr_err
+        df = pd.read_csv(csvfyl)
+        df = df.drop(df.columns[0], axis=1)
+    else:
+        txtdir = '/home/chege/Desktop/curtin_work/run_ionfr/singleLOScomparisons'
+        fyls = os.listdir(txtdir)
+        ras = []
+        decs = []
+        frs = []
+        frs_errs = []
+        for fyl in fyls:
+            # print(fyl)
+            if fyl.split('.')[-1] == 'txt':
+                source = fyl.split('_')[0]
+                ra, dec = run_ionfr.get_radec(source)
+                ras.append(float(ra))
+                decs.append(float(dec))
+                fylpath = txtdir + '/' + fyl
+                fr_value, fr_value_err = get_fr_value(fylpath, hour)
+                frs.append(float(fr_value))
+                frs_errs.append(float(fr_value_err))
+        df = pd.DataFrame(
+            list(zip(ras, decs, frs, frs_errs)),
+            columns=['ra', 'dec', 'fr', 'fr_err'])
+        print('made dataframe with radec and fr values')
+    df = df.dropna(axis=0)
 
     return df
+
 
 def get_center(df):
     bulk_centre_ra = np.mean(df.ra)
     bulk_centre_dec = np.mean(df.dec)
 
-    
     radius = determine_radius(df)
 
     # Recalculate the centre, based on the sources within the radius,
     # and specify the sources to be used for analysis.
-    filtered = np.array([[a, b, c, d] for a, b, c, d
+    filtered = np.array(
+                        [[a, b, c, d] for a, b, c, d
                             in zip(df.ra, df.dec, df.fr, df.fr_err)
                             if abs(a-bulk_centre_ra) < radius
                             and abs(b-bulk_centre_dec) < radius])
@@ -124,12 +140,14 @@ def get_center(df):
 
     return radius, fra, fdec, f_fr, f_fr_err, ra_centre, dec_centre
 
+
 def cropper(matrix, crop_factor=1./np.sqrt(2)):
     length = len(matrix)
     cropped_length = int(length * crop_factor)
     i1 = int((length-cropped_length)/2)
     i2 = length-int((length-cropped_length)/2)
     return matrix[i1:i2, i1:i2]
+
 
 def determine_radius(df):
     # Determine the radius of the primary beam.
@@ -145,59 +163,132 @@ def determine_radius(df):
     try:
         ra_popt, _ = curve_fit(gaussian, ra_bins[:-1], ra_hist, p0=ra_p0)
         dec_popt, _ = curve_fit(gaussian, dec_bins[:-1], dec_hist, p0=dec_p0)
-        radius = np.ceil((2*np.mean([abs(ra_popt[2]), abs(dec_popt[2])]))/2.5)*2.5
+        radius = np.ceil(
+            (2*np.mean([abs(ra_popt[2]), abs(dec_popt[2])]))/2.5)*2.5
         # Check this radius against the extent of source available.
-        if radius > max(ra) - min(ra) or radius > max(dec) - min(dec):
-            radius = max([ra.max() - ra.min(), dec.max() - dec.min()])/2
+        if radius > max(df.ra) - min(df.ra) or radius > max(df.dec) - min(df.dec):
+            radius = max([df.ra.max() - df.ra.min(), df.dec.max() - df.dec.min()])/2
+            print('gaussian fit done')
     except:
         radius = max([df.ra.max() - df.ra.min(), df.dec.max() - df.dec.min()])/2
-    print(radius)
+    # print(radius)
     return radius
 
-def bgplot(radius, ra, dec, fr, fr_err, ra_centre, dec_centre, obsid, hr, interp_method = 'linear', resolution=200):
-    grid_x, grid_y = np.meshgrid(np.linspace(-radius, radius, resolution),
+
+def interpfr(
+            radius, ra, dec, fr, fr_err, ra_centre, dec_centre, obsid, hr,
+            interp_method='linear', resolution=200):
+    grid_x, grid_y = np.meshgrid(
+                                np.linspace(-radius, radius, resolution),
                                 np.linspace(-radius, radius, resolution))
 
     grid_x += ra_centre
     grid_y += dec_centre
 
-    #grid_std = np.flipud(np.fliplr(griddata(np.vstack((ra, dec)).T, stds,
-    #                                (grid_x, grid_y), method=interp_method, fill_value=0)))
-    grid_fr = np.flipud(np.fliplr(griddata(np.vstack((ra, dec)).T, fr,
-                                    (grid_x, grid_y), method=interp_method, fill_value=0)))
+    grid_fr = np.flipud(
+                        np.fliplr(
+                                griddata(
+                                        np.vstack((ra, dec)).T, fr,
+                                        (grid_x, grid_y), method=interp_method,
+                                        fill_value=0)))
 
-    beam_extent = (ra_centre+radius,
+    beam_extent = (
+                    ra_centre+radius,
                     ra_centre-radius,
                     dec_centre-radius,
                     dec_centre+radius)
     print(beam_extent)
-    crop_factor=1./np.sqrt(2)
-    cropped_beam_extent = (ra_centre+radius*crop_factor,
-                    ra_centre-radius*crop_factor,
-                    dec_centre-radius*crop_factor,
-                    dec_centre+radius*crop_factor)
+    crop_factor = 1./np.sqrt(2)
+    cropped_beam_extent = (
+                        ra_centre+radius*crop_factor,
+                        ra_centre-radius*crop_factor,
+                        dec_centre-radius*crop_factor,
+                        dec_centre+radius*crop_factor)
     print(cropped_beam_extent)
-    
-    grid_fr = cropper(grid_fr)
 
+    # grid_fr = cropper(grid_fr)
+
+    return grid_fr, cropped_beam_extent
+
+
+def plot_interp_fr(grid, beam_extent):
     fig, ax = plt.subplots(1, 1)
-    img1 = ax.imshow(grid_fr, extent=cropped_beam_extent, cmap="plasma", origin="lower")
+    img1 = ax.imshow(
+        grid, cmap="plasma", origin="lower")
     ax.set_xlabel("RA [deg]")
     ax.set_ylabel("Dec [deg]")
-    fig.colorbar(img1, ax=ax, format="%.2f",fraction=0.046, pad=0.04)
-    fig.suptitle('Spatial Faraday depth at %shrs %s'%(hr, str(obsid)))
-    plt.savefig('%s_%shrs_ionfr_p3.png' % (obsid, hr))
+    fig.colorbar(img1, ax=ax, format="%.2f", fraction=0.046, pad=0.04)
+    fig.suptitle('Spatial Faraday depth at %shrs %s' % (hr, str(obsid)))
+    plt.savefig('%s_%shrs_ionfr.png' % (obsid, hr))
 
+
+def plane_fit(grid):
+    print('shape', grid.shape)
+    m = grid.shape[0]  # size of the matrix
+
+    X1, X2 = np.mgrid[:m, :m]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(3, 1, 1, projection='3d')
+    jet = plt.get_cmap('jet')
+    ax.plot_surface(X1, X2, grid, rstride=1, cstride=1, cmap=jet, linewidth=0)
+
+    # Regression
+    X = np.hstack((np.reshape(X1, (m*m, 1)), np.reshape(X2, (m*m, 1))))
+    X = np.hstack((np.ones((m*m, 1)), X))
+    YY = np.reshape(grid, (m*m, 1))
+
+    theta = np.dot(np.dot(np.linalg.pinv(np.dot(X.transpose(), X)), X.transpose()), YY)
+
+    ax.scatter(grid[:, 0], grid[:, 1], grid[:, 2], c='r', s=20)
+
+    plane = np.reshape(np.dot(X, theta), (m, m))
+
+    ax = fig.add_subplot(3, 1, 2, projection='3d')
+    ax.plot_surface(X1, X2, plane)
+    # ax.plot_surface(X1, X2, grid, rstride=1, cstride=1, cmap=jet, linewidth=0)
+
+    # Subtraction
+    grid_sub = grid - plane
+    ax = fig.add_subplot(3, 1, 3, projection='3d')
+    ax.plot_surface(X1, X2, grid_sub, rstride=1, cstride=1, cmap=jet, linewidth=0)
+
+    plt.show()
+
+
+def linefit(grid, beam_lim):
+    y = grid[:, 90]
+    print(y.shape)
+    x = np.linspace(beam_lim[2], beam_lim[3], len(y))
+    regplot = sns.regplot(x=x, y=y, color="g")
+    # plt.show()
+    fig = regplot.get_figure()
+    fig.savefig('1065880128_14hrs_1ra_ionfr.png')
+
+
+def line_fit(df):
+    y = grid[:, 90]
+    print(y.shape)
+    x = np.linspace(beam_lim[2], beam_lim[3], len(y))
+    regplot = sns.regplot(x=x, y=y, color="g")
+    # plt.show()
+    fig = regplot.get_figure()
+    fig.savefig('1065880128_14hrs_1ra_ionfr.png')
 
 
 if __name__ == '__main__':
-    #files = sorted(os.listdir(path))
-    #for i in range(len(files)):
-    #obsid = files[i].split('.')[0]
-    #get_obsid_ionfr_txts(1065880128)
+    # files = sorted(os.listdir(path))
+    # for i in range(len(files)):
+    # obsid = files[i].split('.')[0]
+    # get_obsid_ionfr_txts(1065880128)
     obsid = 1065880128
     hr = 14
-    df = make_df(hr)
-    df.to_csv('1065880128_ionfr_p3.csv')
+    df = make_df(hr, csvfyl='1065880128_ionfr_p3.csv')
+    print(df.head())
+    # df.to_csv('1065880128_ionfr_p3.csv')
     radius, fra, fdec, f_fr, f_fr_err, ra_centre, dec_centre = get_center(df)
-    bgplot(radius, fra, fdec, f_fr, f_fr_err, ra_centre, dec_centre, obsid,hr)
+    grid, beam_extent = interpfr(
+        radius, fra, fdec, f_fr, f_fr_err, ra_centre, dec_centre, obsid, hr)
+    plot_interp_fr(grid, beam_extent)
+    # plane_fit(grid)
+    # linefit(grid, beam_extent)
--- vega0/spatscales.py
+++ vega0/spatscales.py
@@ -1,35 +1,33 @@
 import yaml
 from yaml import SafeLoader as SafeLoader
 import numpy as np
-import seaborn as sns; sns.set()
+import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-
-import plotly
-#import plotly.io as pio
-#pio.renderers.default = "pdf"
-#import chart_studio.plotly
-#from plotly.subplots import make_subplots
+# import plotly
+# import plotly.io as pio
+# pio.renderers.default = "pdf"
+# import chart_studio.plotly
+# from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-#import plotly.express as px
+# import plotly.express as px
 
 from scipy.interpolate import griddata
-from scipy import stats
+from scipy.optimize import curve_fit
+# from scipy import stats
 
 import os
-from os import listdir
-from os.path import isfile, join
-
+sns.set()
 '''
-Plot the median amplitude scales of each osurce in a single 
+Plot the mean amplitude scales of each osurce in a single
 yaml file as a function of ra nad dec.
-Interpolate using scipy and try to obtain the background. 
+Interpolate using scipy and try to obtain the background.
 '''
+
 
 def loadfile(data_file):
     with open(data_file, 'r') as f:
-                unpacked = yaml.load(f, Loader=SafeLoader)
-	
+        unpacked = yaml.load(f, Loader=SafeLoader)
     flux = []
     ra = []
     dec = []
@@ -38,37 +36,42 @@ def loadfile(data_file):
     stds = []
     sourcelist = unpacked['sources']
     for source in sourcelist:
-        #print(unpacked['sources'][source]['name'])
+        # print(unpacked['sources'][source]['name'])
         names.append(unpacked['sources'][source]['name'])
         dec.append(unpacked['sources'][source]['dec'])
         ra.append(unpacked['sources'][source]['ra'])
-        flux.append(unpacked['sources'][source]['flux_density']) 
-        ampscales.append(np.nanmedian(unpacked['sources'][source]['amp_scales']))
+        flux.append(unpacked['sources'][source]['flux_density'])
+        ampscales.append(
+            np.nanmean(unpacked['sources'][source]['amp_scales']))
         stds.append(np.nanstd(unpacked['sources'][source]['amp_scales']))
-        #print(unpacked2['sources'][source]['amp_scales'])
-    #ampscales = np.nan_to_num(ampscales)
-    #print(len(ampscales))
+    # ampscales = np.nan_to_num(ampscales)
+    # print(len(ampscales))
 
-    df = pd.DataFrame(list(zip(ra, dec, ampscales, stds, flux, names)), columns=['ra', 'dec', 'ampscales', 'stds', 'flux', 'names'])
-    df = df.dropna(axis=0) #drop all rows with nan value in any column
-    #df = df[(np.abs(stats.zscore(df.stds)) < 3).all(axis=1)] #discard rows with outlier stds (>3*\mu std)
-    df = df[((df.stds - df.stds.mean()) / df.stds.std()).abs() < 3] #filter rows which dont have outlier stds (<3*\mu std)
-    df = df.nlargest(500, 'flux', keep='all') #Get the rows with first n brightest sources.
+    df = pd.DataFrame(
+        list(zip(ra, dec, ampscales, stds, flux, names)),
+        columns=['ra', 'dec', 'ampscales', 'stds', 'flux', 'names'])
+    df = df.dropna(axis=0)  # drop all rows with nan value in any column
+    # df = df[(np.abs(stats.zscore(df.stds)) < 3).all(axis=1)
+    df = df[((df.stds - df.stds.mean()) / df.stds.std()).abs() < 3]  # filter
+    # rows which dont have outlier stds (<3*\mu std)
+    # Get the rows with N brightest sources.
+    df = df.nlargest(900, 'flux', keep='all')
+    print(df.flux)
     return df
+
 
 def get_center(df):
     bulk_centre_ra = np.mean(df.ra)
     bulk_centre_dec = np.mean(df.dec)
 
-    
     radius = determine_radius(df)
 
     # Recalculate the centre, based on the sources within the radius,
     # and specify the sources to be used for analysis.
     filtered = np.array([[a, b, c, d] for a, b, c, d
-                            in zip(df.ra, df.dec, df.ampscales, df.stds)
-                            if abs(a-bulk_centre_ra) < radius
-                            and abs(b-bulk_centre_dec) < radius])
+                        in zip(df.ra, df.dec, df.ampscales, df.stds)
+                        if abs(a-bulk_centre_ra) < radius
+                        and abs(b-bulk_centre_dec) < radius])
 
     fra = filtered[:, 0]
     fdec = filtered[:, 1]
@@ -94,86 +97,110 @@ def determine_radius(df):
     try:
         ra_popt, _ = curve_fit(gaussian, ra_bins[:-1], ra_hist, p0=ra_p0)
         dec_popt, _ = curve_fit(gaussian, dec_bins[:-1], dec_hist, p0=dec_p0)
-        radius = np.ceil((2*np.mean([abs(ra_popt[2]), abs(dec_popt[2])]))/2.5)*2.5
+        radius = np.ceil(
+            (2*np.mean([abs(ra_popt[2]), abs(dec_popt[2])]))/2.5)*2.5
         # Check this radius against the extent of source available.
-        if radius > max(ra) - min(ra) or radius > max(dec) - min(dec):
-            radius = max([ra.max() - ra.min(), dec.max() - dec.min()])/2
+        if radius > max(df.ra) - min(df.ra) or radius > max(df.dec) - min(df.dec):
+            radius = max([df.ra.max() - df.ra.min(), df.dec.max() - df.dec.min()])/2
+            print('gaussian fit done')
     except:
         radius = max([df.ra.max() - df.ra.min(), df.dec.max() - df.dec.min()])/2
-    print(radius)
+    # print(radius)
     return radius
 
 
-def spatial_scales_plot(df, obsid):
+def spatial_scales_plot(ra, dec, ampscales, stds, obsid, beam_lim):
     print('trying plotly')
-    size = df.stds
+    size = stds
     fig = go.Figure(data=[go.Scatter(
-        x=df.ra,
-        y=df.dec,
+        x=ra,
+        y=dec,
         mode='markers',
-        text = df.ampscales,
+        text=ampscales,
         hoverinfo='text',
         marker=dict(
-            color=df.ampscales,
+            color=ampscales,
             colorscale='Magma_r',
-            size = df.stds,
-            #sizemode = 'diameter',
+            size=stds,
+            # sizemode = 'diameter',
             showscale=True,
-            sizeref = 2. * max(size) / (6**2)
+            sizeref=2. * max(size) / (6**2)
             )
-)])
-    fig.update_layout(title = 'colour=median, size=std')
-    fig.update_xaxes(autorange="reversed")
-    #fig.show()
-    fig.write_image('%s_amps_500.png' % (obsid))
+    )])
+    fig.update_layout(
+                    title='colour=mean, size=std',
+                    xaxis=dict(range=[beam_lim[0], beam_lim[1]], title='Ra [deg]'),
+                    yaxis=dict(range=[beam_lim[2], beam_lim[3]], title='Dec [deg]'))
 
-def bgplot(radius, ra, dec, ampscales, stds, ra_centre, dec_centre, obsid, interp_method = 'linear', resolution=200):
-    grid_x, grid_y = np.meshgrid(np.linspace(-radius, radius, resolution),
-                                np.linspace(-radius, radius, resolution))
+    # fig.update_xaxes(autorange="reversed")
+    # fig.show()
+    fig.write_image('%s_mean_amps_900.png' % (obsid))
+
+
+def bgplot(
+            radius, ra, dec, ampscales, stds, ra_centre, dec_centre, obsid,
+            interp_method='linear', resolution=900):
+    grid_x, grid_y = np.meshgrid(
+        np.linspace(-radius, radius, resolution),
+        np.linspace(-radius, radius, resolution))
 
     grid_x += ra_centre
     grid_y += dec_centre
 
-    grid_std = np.flipud(np.fliplr(griddata(np.vstack((ra, dec)).T, stds,
-                                    (grid_x, grid_y), method=interp_method, fill_value=0)))
-    grid_med = np.flipud(np.fliplr(griddata(np.vstack((ra, dec)).T, ampscales,
-                                    (grid_x, grid_y), method=interp_method, fill_value=1)))
+    grid_std = np.fliplr(
+                        griddata(
+                                np.vstack((ra, dec)).T, stds,
+                                (grid_x, grid_y), method=interp_method,
+                                fill_value=0))
+    grid_med = np.fliplr(
+                        griddata(
+                                np.vstack((ra, dec)).T, ampscales,
+                                (grid_x, grid_y), method=interp_method,
+                                fill_value=1))
 
-    beam_extent = (ra_centre+radius,
+    beam_extent = (
+                    ra_centre+radius,
                     ra_centre-radius,
                     dec_centre-radius,
-                    dec_centre+radius)
-    print(beam_extent)
-    crop_factor=1./np.sqrt(2)
-    cropped_beam_extent = (ra_centre+radius*crop_factor,
-                    ra_centre-radius*crop_factor,
-                    dec_centre-radius*crop_factor,
-                    dec_centre+radius*crop_factor)
-    print(cropped_beam_extent)
-    
+
+                    dec_centre+radius
+                )
+    # print(beam_extent)
+    crop_factor = 1./np.sqrt(2)
+    cropped_beam_extent = (
+                        ra_centre+radius*crop_factor,
+                        ra_centre-radius*crop_factor,
+                        dec_centre-radius*crop_factor,
+                        dec_centre+radius*crop_factor)
+    print(obsid, 'cropped beam limits', cropped_beam_extent)
+
     grid_med = cropper(grid_med)
     grid_std = cropper(grid_std)
-    #grid_ddec = np.flipud(np.fliplr(griddata(np.vstack((df.ra, df.dec)).T, df.ampscales,
-    #                                        (grid_x, grid_y), method=interp_method, fill_value=0)))
+    spatial_scales_plot(df.ra, df.dec, df.ampscales, df.stds, obsid, cropped_beam_extent)
 
-
-    
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 10))
-    fig.suptitle('%s amplitude scales: median (left), standard deviation (right)'%(obsid))
-    img1 = ax1.imshow(grid_med, extent=cropped_beam_extent, cmap="plasma", origin="lower")
-    fig.colorbar(img1, ax=ax1, format="%.2f",fraction=0.046, pad=0.04)
-    img2 = ax2.imshow(grid_std, extent=cropped_beam_extent, cmap="plasma", origin="lower")
-    fig.colorbar(img2, ax=ax2, format="%.2f",fraction=0.046, pad=0.04)
-    plt.savefig('%s_cubic_interp500.png' % (obsid)) 
+    fig.suptitle(
+        '%s amplitude scales: mean (left), standard deviation \
+        (right)' % (obsid))
+    img1 = ax1.imshow(
+        grid_med, extent=cropped_beam_extent, cmap="plasma", origin="lower")
+    fig.colorbar(img1, ax=ax1, format="%.2f", fraction=0.046, pad=0.04)
+    img2 = ax2.imshow(
+        grid_std, vmin=0.0, vmax=0.4, extent=cropped_beam_extent, cmap="plasma", origin="lower")
+    fig.colorbar(img2, ax=ax2, format="%.2f", fraction=0.046, pad=0.04)
+    plt.savefig('%smean_linear_interp900.png' % (obsid))
     '''
     fig, ax = plt.subplots(1, 1)
-    img1 = ax.imshow(grid_std, vmin=0.0, vmax=0.8, extent=cropped_beam_extent, cmap="plasma", origin="lower")
+    img1 = ax.imshow(grid_std, vmin=0.0, vmax=0.8, extent=cropped_beam_extent,
+        cmap="plasma", origin="lower")
     ax.set_xlabel("RA [deg]")
     ax.set_ylabel("Dec [deg]")
     fig.colorbar(img1, ax=ax, format="%.2f",fraction=0.046, pad=0.04)
     fig.suptitle('standard deviation of amplitude scales')
-    plt.savefig('%s_stdamps_500brightest.png' % (obsid))
+    plt.savefig('%s_stdamps_900brightest.png' % (obsid))
     '''
+
+
 def cropper(matrix, crop_factor=1./np.sqrt(2)):
     length = len(matrix)
     cropped_length = int(length * crop_factor)
@@ -181,15 +208,18 @@ def cropper(matrix, crop_factor=1./np.sqrt(2)):
     i2 = length-int((length-cropped_length)/2)
     return matrix[i1:i2, i1:i2]
 
+
 if __name__ == "__main__":
     path = '/home/chege/Desktop/curtin_work/vega/'
     files = sorted(os.listdir(path))
-    for i in range(len(files[:3])):
-        obsid = files[i].split('.')[0]
-        data_file = '%s' % (path+files[i])
-        df = loadfile(data_file) 
-        radius, fra, fdec, f_ampscales, f_stds, ra_centre, dec_centre = get_center(df)
-        #spatial_scales_plot(df, obsid)
-        bgplot(radius, fra, fdec, f_ampscales, f_stds, ra_centre, dec_centre, obsid, interp_method = 'cubic')
-
-
+    for i in files[:55]:
+        obsid = i.split('.')[0]
+        data_file = '%s' % (path+i)
+        df = loadfile(data_file)
+        radius, fra, fdec, f_ampscales, f_stds, ra_centre, dec_centre \
+            = get_center(df)
+        # spatial_scales_plot(df.ra, df.dec, df.ampscales, df.stds, obsid)
+        bgplot(
+            radius, fra, fdec, f_ampscales, f_stds, ra_centre, dec_centre,
+            obsid, interp_method='linear'
+            )
